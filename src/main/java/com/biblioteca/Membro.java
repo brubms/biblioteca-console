c90a00957b6de68d97d9eaec5a6d644c489e3633
brubms <brunoberna21@gmail.com>
2026-09-22 20:52:39 -0300

    refactor: remove o contador manual em Mmebro delegando auto incremento para o JPA

--- src/main/java/com/biblioteca/Membro.java
+++ src/main/java/com/biblioteca/Membro.java
@@ -8,102 +8,110 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
-@Entity 
+@Entity
+// essa anotação diz qual vai ser o nome da tabela!
 @Table(name = "membros")
 public class Membro {
 
-    //Atributos:
-    @Column (name = "nome", nullable = false)
+    // Atributos:
+    /*
+    *nullable = false -> quer referênciar que aquele atributo
+    *não pode faltar!
+    */
+    @Column(name = "nome", nullable = false)
     private String nome;
-
+    /*
+     * nesse caso o unique = true -> quer dizer que não podemos
+     * ter dois emails cadastrados no banco de dados que sejam iguais,
+     * torna se então único para o banco de dados
+     */
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Id 
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column (name = "livros_emprestados")
+    @Column(name = "livros_emprestados")
     private int livrosEmprestados = 0;
 
-    @Column (name = "ativo")
+    @Column(name = "ativo")
     private boolean ativo = true;
-    
 
-    public Membro(){
+    public Membro() {
 
     }
 
-    //Construtor:
-    public Membro(String nome, String email){
+    // Construtor:
+    public Membro(String nome, String email) {
         this.nome = nome;
         this.email = email;
-       
+
     }
-       
 
-    //Método para pegar livros!
-    public void pegarLivro(){
+    // Método para pegar livros!
+    public void pegarLivro() {
 
-       if(livrosEmprestados>=3){
+        if (livrosEmprestados >= 3) {
             System.out.println("Limite máximo atingido");
             return;
-        }else{
+        } else {
             livrosEmprestados++;
-        System.out.println(getNome() + " Pegou livro");
+            System.out.println(getNome() + " Pegou livro");
 
         }
-        
-        
+
     }
 
-    //método para devolução
-    public void devolverLivro(){
-        if(livrosEmprestados >=1){
+    // método para devolução
+    public void devolverLivro() {
+        if (livrosEmprestados >= 1) {
             livrosEmprestados--;
-            System.out.println(getNome()  +" devolveu o livro!");
-         
-        }else{
+            System.out.println(getNome() + " devolveu o livro!");
+
+        } else {
             System.out.println("Não existem livros para devolver!");
         }
     }
 
-    public void desativar(){
-        if(livrosEmprestados == 0){
+    public void desativar() {
+        if (livrosEmprestados == 0) {
             ativo = false;
             System.out.println(getNome() + " Desativado");
-        }else{
+        } else {
             System.out.println("Não consigo desativar, Erro!!");
         }
     }
-    public void reativar(){
-            ativo = true;
-            System.out.println(getNome() +" Ativo");
+
+    public void reativar() {
+        ativo = true;
+        System.out.println(getNome() + " Ativo");
 
     }
 
-    public String getNome(){
-       return this.nome;
-       
+    public String getNome() {
+        return this.nome;
+
     }
-    public String getEmail(){
-       return this.email;
+
+    public String getEmail() {
+        return this.email;
     }
-    public int getId(){
+
+    public int getId() {
         return this.id;
     }
-    public int getLivrosEmprestados(){
+
+    public int getLivrosEmprestados() {
         return this.livrosEmprestados;
     }
 
-    public boolean isAtivo(){
+    public boolean isAtivo() {
         return this.ativo;
     }
 
     @Override
-    public String toString(){
-        return  getNome() + "|" + getEmail() + "|"  + getLivrosEmprestados() + "|" + (isAtivo() ? "sim" : "nao");
+    public String toString() {
+        return getNome() + "|" + getEmail() + "|" + getLivrosEmprestados() + "|" + (isAtivo() ? "sim" : "nao");
     }
 }
-
-
