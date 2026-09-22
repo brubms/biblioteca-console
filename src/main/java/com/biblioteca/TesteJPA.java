b3c99c063beace199b01e0531a846e9dae587ee1
brubms <brunoberna21@gmail.com>
2026-09-22 20:56:37 -0300

    teste: valida operacoes com persist, find e dirty checking com JPA

--- src/main/java/com/biblioteca/TesteJPA.java
+++ src/main/java/com/biblioteca/TesteJPA.java
@@ -6,12 +6,39 @@ import jakarta.persistence.Persistence;
 
 public class TesteJPA {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("biblioteca-pu");
-        EntityManager em = emf.createEntityManager();
 
-        System.out.println("Hibernate conectou ao MySQL");
+        
+        /*
+         * O EntityManagerFactory só existe um no programa que abre ao aplicação ser
+         * criada e fecha quando sistema encerra!
+         */
+        //EntityManagerFactory emf = Persistence.createEntityManagerFactory("biblioteca-pu");
+        /*
+        EntityManager é criado para cada úsuario, ao cada um solicitar algo 
+        ele é criado
+        */
+        //EntityManager em = emf.createEntityManager();
+
+        //System.out.println("Hibernate conectou ao MySQL");
+
+        //em.close();
+        //emf.close();
+
+        EntityManager em = FabricaEntityManager.getEntityManager();
+        
+        
+
+        em.getTransaction().begin();
+        Membro membro = em.find(Membro.class, 6);
+        membro.pegarLivro();
+        em.getTransaction().commit();
+
+        System.out.println("Encontrei: " + membro.getNome());
+
+        
 
         em.close();
-        emf.close();
+        FabricaEntityManager.fechar();
+    
     }
 }
